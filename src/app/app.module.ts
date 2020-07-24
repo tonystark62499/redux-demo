0c435d248ab7809ffebe1c9dcf94912e04162181
Yash Patel <yash.patel@alpsinfo.net>
2020-07-24 17:52:44 +0530

    initial commit of redux

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-@NgModule({
+import { NgRedux, NgReduxModule, DevToolsExtension } from '@angular-redux/store';
+import { IAppState, rootReducer, INITIAL_STATE } from './store';
+import { TodoOverviewComponent } from './todo-overview/todo-overview.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { FormsModule } from '@angular/forms';
+@NgModule({ 
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoOverviewComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgReduxModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+  constructor (ngRedux: NgRedux<IAppState>, devTools: DevToolsExtension) {
+    var enhancers = isDevMode() ? [devTools.enhancer()]: [];
+    ngRedux.configureStore(rootReducer, INITIAL_STATE, [],enhancers);
+}
+}
